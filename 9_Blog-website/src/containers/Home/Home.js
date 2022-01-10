a3dc01182e8399aa0d3cc96af732280d33e51bae
Pierre Chevallier <pierre.chevallier1@etu.univ-grenoble-alpes.fr>
2022-01-10 22:05:47 +0100

    Add articles to the home page

--- 9_Blog-website/src/containers/Home/Home.js
+++ 9_Blog-website/src/containers/Home/Home.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import Card from "../../components/Card/Card";
-import { useSelector, useDispatch } from "redux";
-import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { getArticles } from "../../redux/articles/articleReducer";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
+  const { articles } = useSelector((state) => ({
+    ...state.articleReducer,
+  }));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (articles.length === 0) {
+      dispatch(getArticles());
+    }
+  }, []);
+
   return (
     <>
       <h1 className="home-title">Articles</h1>
       <div className="cards-container">
-        <Card>
-          <h2>Test article</h2>
-        </Card>
+        {articles.map((item) => {
+          return (
+            <Card key={uuidv4()}>
+              <h2>{item.title}</h2>
+              <a href="#">Read the article</a>
+            </Card>
+          );
+        })}
       </div>
     </>
   );
